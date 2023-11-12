7d1eb24bd5b443430fc120c9df984bae00baf8a1
ExpliuM <alex1309@gmail.com>
2023-11-12 13:55:59 +0100

    renaming

--- mac/brew/list.sh
+++ mac/brew/list.sh
@@ -2,5 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-brew list >$SCRIPT_DIR/list
-brew list --cask >$SCRIPT_DIR/cask_list
+mkdir -p $SCRIPT_DIR/lists
+
+brew list >$SCRIPT_DIR/lists/list
+brew list --cask >$SCRIPT_DIR/lists/cask_list
